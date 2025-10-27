a88192c79c207f2f810fb4117bf803794f1e1e25
bowornnanza-glitch <bowornnanza@gmail.com>
2025-10-27 22:06:15 +0700

    Fix: Update certificate gallery to use existing images

--- app/activities/certificates/page.tsx
+++ app/activities/certificates/page.tsx
@@ -49,21 +49,19 @@ export default function CertificatesPage() {
   }, [selectedImage])
 
   const galleryImages: string[] = [
-    '/images/1.png',
-    '/images/2.png',
-    '/images/3.png',
-    '/images/4.png',
-    '/images/5.png',
-    '/images/6.png',
-    '/images/7.png',
-    '/images/8.png',
-    '/images/9.png',
-    '/images/10.png',
-    '/images/11.png',
-    '/images/12.png',
-    '/images/13.png',
     '/images/14.png',
-    '/images/15.jpg'
+    '/images/15.jpg',
+    '/images/DP1.jpg',
+    '/images/TD1.jpg',
+    '/images/TD2.jpg',
+    '/images/pt1.jpg',
+    '/images/pt2.jpg',
+    '/images/pt3.jpg',
+    '/images/pt4.jpg',
+    '/images/pt5.jpg',
+    '/images/pt6.jpg',
+    '/images/pt7.jpg',
+    '/images/pt8.jpg'
   ]
 
   return (
